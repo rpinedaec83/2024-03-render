a22b5c524c23ff92d5267812a84b9d49265b1004
Roberto Pineda <rpineda@x-codec.net>
2024-07-11 20:08:30 -0500

    cuarto despliegue

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,5 +2,5 @@ module.exports = {
     HOST: "0.0.0.0",
     PORT: 27017,
     DB: "auth_db",
-    URI: "mongodb+srv://rpineda:Ysb39VAkymQPyKl4@cluster0.6znffma.mongodb.net/"
+    URI: process.env.mongoURI
 };
